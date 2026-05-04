27b6fa6d4c9fd0d006843da8024403bba39607f1
LelinskiM <241956@student.uek.krakow.pl>
2026-05-04 15:32:25 +0200

    2026-05-04 ML

--- Ceneo_scraper.py
+++ Ceneo_scraper.py
@@ -2,6 +2,10 @@
 import os
 import json
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 
 # get product page / code
@@ -9,11 +13,13 @@ product_code = input("Input product code: ")
 page = 1
 
 next =True
-headers =(
+headers ={
     "Host":"www.ceneo.pl",
     "Cookie":"rc=igdamb4ThOR9TJV1wnnHj0peFsMflKvy5/lp9BN0GICfk5H4KXZZ6cJnqS0eypifqC3MZeoTduSk6rO+Dotsr41HXVTMsgurVnSzz257svLLQS2sjY9sN6gtzGXqE3bkpOqzvg6LbK+NR11UzLILq1Z0s89ue7Lyo3PdJGdIi2GJEbcwQDURfTI9fGwGMD364+NrKUZBiCscG0vEN/6qtg==; domain=.ceneo.pl; expires=Sun, 19-Jul-2026 13:21:44 GMT; path=/; secure; HttpOnly; SameSite=Lax",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
-    )
+    "Accept-Language": "pl-PL,pl;q=0.9"
+}
+
 all_opinions =[]
 
 ##### start of while loop to repeat for all pages of opinions
@@ -22,15 +28,28 @@ while next:
     url = f"https://www.ceneo.pl/{product_code}/opinie-{page}"
     print(url)
 
+    # prepselenium
+    path_to_driver = "D:\\selenium\\chromedriver-win64\\chromedriver.exe"
+    s = Service(path_to_driver)
+    driver = webdriver.Chrome(service=s)
+    driver.get(url)
+    driver.maximize_window()
+    driver.find_element(by="xpath", value='//*[@id="js_cookie-consent-general"]/div/div[2]/button[1]').click()
+
     # sent request to url
     response = requests.get(url)
+    print(response)
 
     #fetch product name
+    #print(response.status_code)
+    #print(response.text[:2000])
+    #response = requests.get(url, headers=headers)
+
     page_dom = BeautifulSoup(response.text, "html.parser")
 
-    Product_Name = page_dom.select_one('h1').get_text()
+    Product_Name = page_dom.select_one('h1').get_text() ###ERROR ON THIS LINE- Need to use solenium
 
-    Product_Name = page_dom.find('h1').get_text() if not Product_Name else Product_Name
+    Product_Name = page_dom.find('h1').get_text()
 
     #fetch all opinions
     Opinions = page_dom.select("div.js_product-review:not(.user-post--highlight)")
@@ -55,13 +74,18 @@ while next:
 
     # check for page
     next = True if page_dom.select_one("button.pagination__next") else False
-    if next: page += 1
+    if next: 
+        page += 1 
+        driver.find_element (by="xpath", value = '//*[@id="reviews"]/div/div[6]/button[4]').click()
 
 ##### end of while loop
-
+print(all_opinions)
 #save opinions
 if not os.path.exists("./opinions"):
     os.mkdir("./opinions")
 
 with open(f"./opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
     json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+
+
+print("done !!!")
